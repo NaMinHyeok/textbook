881100b1c784a84ec79c2e2046fb530851b13967
naminhyeok <nmh9097@gmail.com>
2022-10-06 15:58:10 +0900

    강의록 6-2 예제 2 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 import './index.css'
-import MemoList from "./chapter_08/MemoList";
+import NoRendering from "./chapter_08/NoRendering";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
-        <MemoList />
+        <NoRendering />
     </>
 );
