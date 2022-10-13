9d3ef60d3ca93a3fa54e5c8d8f69ec9b15d094bc
naminhyeok <nmh9097@gmail.com>
2022-10-13 13:53:46 +0900

    강의록 7-1 예제2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 import './index.css'
-import NumberList from "./chapter_09/NumberList";
+import AttendanceBook from "./chapter_09/AttendanceBook";
 
-const numbers = [1,2,3,4,5];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div>
-        <NumberList numbers={numbers}/>
+        <AttendanceBook/>
     </div>
 );
