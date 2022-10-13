e5203577326e47862b625073f17bde596fa20362
naminhyeok <nmh9097@gmail.com>
2022-10-13 14:02:53 +0900

    강의록 7-1 예제3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 import './index.css'
-import AttendanceBook from "./chapter_09/AttendanceBook";
+import ToDoList from "./chapter_09/ToDoList";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div>
-        <AttendanceBook/>
+        <ToDoList/>
     </div>
 );
