deef63b04501099558812d7b64c3686f5729fced
naminhyeok <nmh9097@gmail.com>
2022-10-30 20:47:37 +0900

    실습일지 9-1 과제

--- src/chapter_11/Dialog.jsx
+++ src/chapter_11/Dialog.jsx
@@ -1,28 +1,79 @@
 import React from "react";
 import './Dialog.css'
 
+
 function Border(props){
     return <div className={'FancyBorder FancyBorder-' + props.color}>
         {props.children}
     </div>
 }
-
 function Dialog(props){
     return (
-        <Border color="blue">
+        <Border color="black">
+            <div className={'navbar navbar-' + props.color}></div>
             <h1 className="Dialog-title">{props.title}</h1>
             <p className="Dialog-message">{props.message}</p>
+            {props.boolean ? <button>{props.button}</button> : ''}
         </Border>
     );
 }
 
 function WelcomeDialog(props){
     return (
-        <Dialog
-            title="Welcome"
-            message="Hello React"
-        />
+        <>
+            <Dialog
+                color="green"
+                title="Welcome"
+                message="Hello React"
+                boolean="0"
+                button="Join"
+            />
+        </>
     );
 }
 
-export default WelcomeDialog;
+function ErrorDialog(props){
+    return (
+        <Dialog
+            color="red"
+            title="Error"
+            message="This is ErrorDialog"
+            boolean="true"
+            button="backspace"
+            />
+    );
+}
+
+function NoticeDialog(props){
+    return (
+        <Dialog
+            color="blue"
+            title="Notice"
+            message="this is Notice dialog"
+            // boolean="false" 를 사용하면 button이 화면에 출력되면 안되는데 출력됨 아마도 true false를 인식하는게 아닌거같음..
+            />
+    );
+}
+
+function WarningDialog(props){
+    return (
+        <Dialog
+            color="yellow"
+            title="Warning"
+            message="this is WarningDialog"
+            />
+    );
+}
+
+function App(){
+    return (
+        <>
+            <WarningDialog/>
+            <WelcomeDialog/>
+            <ErrorDialog/>
+            <NoticeDialog/>
+        </>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 import './index.css'
-import App from "./client/App";
+import App from "./chapter_11/Dialog";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // stricMode 애플리케이션 내의 잠재적인 문제를 알아 내기 위한 도구
