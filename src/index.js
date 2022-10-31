31fe44f84600fc971cb20083470f64b651ee10c5
naminhyeok <nmh9097@gmail.com>
2022-10-31 15:51:42 +0900

    수업내용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
 import './index.css'
-import App from "./chapter_11/Dialog";
+import PageColor from "./chapter_12/PageColor";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // stricMode 애플리케이션 내의 잠재적인 문제를 알아 내기 위한 도구
     <React.StrictMode> 
-        <App />
+        <PageColor />
     </React.StrictMode>
 );
